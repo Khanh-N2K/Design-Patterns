0ebdbd1ac2320ab139ded6c50adae864ced9d9ab
Nguyen Nam Khanh <nguyennamkhanh250803@gmail.com>
2025-08-14 14:38:29 +0700

    [add]
    add singleton pattern

---
+++ Assets/DesignPatterns/Singleton/Scripts/Core/Singleton.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+namespace N2K.Singleton
+{
+    public class Singleton<T> : MonoBehaviour where T : Singleton<T>
+    {
+        [Header("=== SINGLETON ===")]
+
+        [Header("Data")]
+        public static T Instance { get; private set; }
+
+        public virtual void Initialize()
+        {
+            if(Instance != null)
+            {
+                Debug.LogError($"There's more than 1 instance of {typeof(T)} existed!");
+                return;
+            }
+
+            Instance = (T) this;
+        }
+    }
+}
